404a9e3d6369990139d339c4e4129b8013dba099
Jason <121495300+jason21715@users.noreply.github.com>
2023-04-09 07:28:50 -0700

    Add buildTree function and filterArray helper function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 class Node {
-  constructor(value, leftChild, rightChild) {
+  constructor(value, leftChild = null, rightChild = null) {
     this.value = value;
     this.leftChild = leftChild;
     this.rightChild = rightChild;
@@ -8,15 +8,14 @@ class Node {
 
 class Tree {
   constructor(array) {
-    this.array = array;
-    this.root = buildTree();
+    this.root = buildTree(filterArray(array));
   }
 }
 
-function buildTree(array) {
-  // removes duplicate numbers and sorts the array afterwards
+//helper function to remove dupes and sort numbers
+function filterArray(arr) {
   let duplicateFilter = [];
-  array.forEach((num) => {
+  arr.forEach((num) => {
     if (!duplicateFilter.includes(num)) {
       duplicateFilter.push(num);
     }
@@ -25,21 +24,36 @@ function buildTree(array) {
     return a - b;
   });
   return sortedArray;
-  ///////////////////////////////////////////////////////////
 }
 
-buildTree([2, 2, 2, 1, 3, 5, 6, 2, 7, 5, 6]);
+function buildTree(array, start = 0, end = array.length - 1) {
+  //turn it into a balanced binary tree full of Node objects appropriately placed
+  //Node objects have (value, leftChild, rightChild);
+  //The buildTree function should return the level-0 root node(midpoint).
+  let midPoint = Math.floor((start + end) / 2);
+  //recursion
+  if (start > end) {
+    return null;
+  }
+  let newNode = new Node(array[midPoint], null, null);
+  newNode.leftChild = buildTree(array, start, midPoint - 1);
+  newNode.rightChild = buildTree(array, midPoint + 1, end);
+  return newNode;
+}
 
-// Binary Tree Visualizer
-// const prettyPrint = (node, prefix = "", isLeft = true) => {
-//   if (node === null) {
-//     return;
-//   }
-//   if (node.right !== null) {
-//     prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-//   }
-//   console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-//   if (node.left !== null) {
-//     prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-//   }
-// };
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.rightChild !== null) {
+    prettyPrint(node.rightChild, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+  if (node.leftChild !== null) {
+    prettyPrint(node.leftChild, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+const testArray = [2, 2, 2, 1, 3, 5, 6, 2, 7, 5, 6];
+const tree = new Tree(testArray);
+prettyPrint(tree.root);
