7492e50aa9dbb504953f28daf57002ef76025a59
Jason <121495300+jason21715@users.noreply.github.com>
2023-04-09 22:09:35 -0700

    Add insert function

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
+class Tree {
+  constructor(array) {
+    this.root = buildTree(filterArray(array));
+  }
+
+  insert(value) {
+    let currentRoot = this.root;
+    //doesn't allow duplicates
+    if (value === currentRoot.value) {
+      return null;
+    }
+
+    while (currentRoot.leftChild !== null || currentRoot.rightChild !== null) {
+      if (value < currentRoot.value) {
+        //keeps setting the new root until the root has no left/right child
+        currentRoot.leftChild === null
+          ? (currentRoot.leftChild = new Node(value))
+          : (currentRoot = currentRoot.leftChild);
+      } else if (value > currentRoot.value) {
+        currentRoot.rightChild === null
+          ? (currentRoot.rightChild = new Node(value))
+          : (currentRoot = currentRoot.rightChild);
+      }
+    }
+
+    if (value < currentRoot.value) {
+      currentRoot.leftChild = new Node(value);
+    } else if (value > currentRoot.value) {
+      currentRoot.rightChild = new Node(value);
+    }
+  }
+
+  delete(value) {}
+}
+
 class Node {
   constructor(value, leftChild = null, rightChild = null) {
     this.value = value;
     this.leftChild = leftChild;
     this.rightChild = rightChild;
   }
-}
-
-class Tree {
-  constructor(array) {
-    this.root = buildTree(filterArray(array));
-  }
-}
-
-//helper function to remove dupes and sort numbers
-function filterArray(arr) {
-  let duplicateFilter = [];
-  arr.forEach((num) => {
-    if (!duplicateFilter.includes(num)) {
-      duplicateFilter.push(num);
-    }
-  });
-  let sortedArray = duplicateFilter.sort(function (a, b) {
-    return a - b;
-  });
-  return sortedArray;
 }
 
 function buildTree(array, start = 0, end = array.length - 1) {
@@ -41,6 +56,21 @@ function buildTree(array, start = 0, end = array.length - 1) {
   return newNode;
 }
 
+//helper functions
+//remove dupes and sort numbers
+function filterArray(arr) {
+  let duplicateFilter = [];
+  arr.forEach((num) => {
+    if (!duplicateFilter.includes(num)) {
+      duplicateFilter.push(num);
+    }
+  });
+  let sortedArray = duplicateFilter.sort(function (a, b) {
+    return a - b;
+  });
+  return sortedArray;
+}
+//visualize tree
 const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node === null) {
     return;
@@ -56,4 +86,10 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 
 const testArray = [2, 2, 2, 1, 3, 5, 6, 2, 7, 5, 6];
 const tree = new Tree(testArray);
+
+tree.insert(4);
+tree.insert(0);
+tree.insert(9);
+tree.insert(2.5);
+tree.insert(12);
 prettyPrint(tree.root);
